fd24d12a08a6ab77873d16fb33a64734b3d1c9c8
stephen <schwahn3@gmail.com>
2014-04-10 18:34:54 -0400

    Fixing and commenting the plugin library. The problem was in the tts part of the library where it was returning from the foreground thread before completion.

--- app/src/main/java/speak/me/plugin/SpeakMeListener.java
+++ app/src/main/java/speak/me/plugin/SpeakMeListener.java
@@ -14,42 +14,42 @@ import android.util.Log;
 import java.util.ArrayList;
 
 /**
- * Created by stephen on 3/27/14.
+ * Retrieves speech from the user and provides different methods for handling errors that arise
+ * from it. Note that all Speech-To-Text operations must occur on the Main Application thread, and
+ * therefore, we must discover it.
  */
 public class SpeakMeListener {
 
-    // Set up the binding and the binder class
-//    private IBinder mBinder = new SpeechBinder();
+    private int silenceTimout;
     private Service parent = null;
     private Handler mainThread = null;
 
+    /**
+     * Sets the Service's parent and discovers the application main thread, which is necessary for
+     * performing Speech-To-Text operations.
+     *
+     * @param parent
+     */
     public SpeakMeListener(Service parent) {
         this.parent = parent;
         mainThread = new Handler(parent.getApplication().getMainLooper());
+        silenceTimout = 3000;
     }
 
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//
-//        return mBinder;
-//    }
-//
-//    public class SpeechBinder extends Binder {
-//        public void setupListener(Service parent) {
-//            SpeakMeListener.this.parent = parent;
-//        }
-//
-//       public String[] queryUser() throws InvalidSpeechException {
-//           callback.completed = false;
-//           parent.getMainLooper()
-//           return getInput();
-//       }
-//    }
 
     // Now implement the speech recognition stuff
     private SpeechRecognizer speechService;
 
+    /**
+     * Returns a String[] of possible user speech recommendations. It runs on the main thread and
+     * uses a callback mechanism to allow this method to block during each operation. This means
+     * that it will not return until the item in the background has been completed.
+     * @return
+     * @throws InvalidSpeechException
+     */
     public String[] queryUser() throws InvalidSpeechException {
+        // Set the callback to not be completed and then get user feedback by poting
+        // onto the main thread.
         callback.completed = false;
         mainThread.post(new Runnable() {
             @Override
@@ -63,6 +63,8 @@ public class SpeakMeListener {
             }
         });
 
+        // Now that this operation has been posted, wait until the operation is complete.
+        // To determine this, the completed variable in the callback is checked.
         while (!callback.completed) {
             synchronized (this) {
                 try {
@@ -73,6 +75,9 @@ public class SpeakMeListener {
             }
         }
 
+        // Now, we also have to cancel and destroy the service on the main thread. Neither
+        // cancel nor destroy works by itself, so both must be called in order to not have a
+        // memory leak upon leaving the plugin.
         mainThread.post(new Runnable() {
             @Override
             public void run() {
@@ -81,21 +86,31 @@ public class SpeakMeListener {
             }
         });
 
+        // If an error occurred, then the error flag in the callback will be set. Simply
+        // throw an Exception and let the caller deal with it.
         if (callback.error) {
             throw new InvalidSpeechException();
         }
+
+        // The results of the operation are located in the callback results.
         return callback.results;
     }
 
+    /**
+     * Helper method for queryUser(), this method is invoked in the main thread and is responsible
+     * for starting the speech service that is used to query the user.
+     */
     private void getInput() {
         // First initialize the listener
         speechService = SpeechRecognizer.createSpeechRecognizer(parent);
         speechService.setRecognitionListener(new SpeakMeRecognizer());
 
-        // Setup the
+        // Set up the intent for 5 results in Dictation mode and no extra prompt.
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        //Log.d("SPEAKME LISTENER", "Package:" + parent.getClass().getPackage().getName());
+//        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, silenceTimout);
+//        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, silenceTimout);
+//        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, silenceTimout);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "speak.me");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, false);
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
@@ -104,12 +119,21 @@ public class SpeakMeListener {
     }
 
     private SynchronizedCallback callback = new SynchronizedCallback();
+
+    /**
+     * Contains the volatile variables needed to send dat across threads. This is used to block
+     * methods and retrieve data from the main thread back to this background thread.
+     */
     private class SynchronizedCallback {
         public volatile boolean completed = false;
         public volatile boolean error = false;
         public volatile String[] results;
     }
 
+    /**
+     * Sets the callback variables appropriately onError() and onResult(). This class is used in
+     * on the main thread to detect and respond to speech.
+     */
     private class SpeakMeRecognizer implements RecognitionListener {
 
         @Override
@@ -130,6 +154,8 @@ public class SpeakMeListener {
         @Override
         public void onError(int i) {
             Log.d("SPEAKME LISTENER", "In onOnError");
+
+            // If there is an error, set the completed and boolean flags
             callback.completed = true;
             callback.error = true;
         }
@@ -137,9 +163,14 @@ public class SpeakMeListener {
         @Override
         public void onResults(Bundle bundle) {
             Log.d("SPEAKME LISTENER", "In onResults");
+
+            // Retrieves the list of possible results.
+            ArrayList<String> results = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            // If there are results available, set the completed, error, and results
+            // of the callback.
             callback.completed = true;
             callback.error = false;
-            ArrayList<String> results = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
             callback.results = results.toArray(new String[]{});
         }
 
@@ -150,6 +181,10 @@ public class SpeakMeListener {
         public void onEvent(int i, Bundle bundle) {}
     }
 
+    /**
+     * Ensure that the parent and mainThread references are destroyed, so that the garbage collector
+     * can remove them if necessary. This prevents memory leaks.
+     */
     public void destroy() {
         parent = null;
         mainThread = null;
--- app/src/main/java/speak/me/plugin/SpeakMeReader.java
+++ app/src/main/java/speak/me/plugin/SpeakMeReader.java
@@ -13,18 +13,24 @@ import android.util.Log;
 import java.util.HashMap;
 import java.util.Locale;
 
+/**
+ * Controls the SpeakMePlugin's text-to-speech reader, and deals with the utterances
+ * necessary to create a blocking situation.
+ */
 public class SpeakMeReader {
 	private TextToSpeech tts;
     private Service parent;
 
-    private boolean speaking =  false;
+    private boolean speaking = false;
     private String utteranceID = "utterance_id_speech";
 
+    /**
+     * Creates a new android.speech.tts.TextToSpeech object and sets it up. This involves making a
+     * new UtteranceListener, that will set a member boolean to true/false based on whether the
+     * tts is currently speaking or is finished.
+     * @param parent Refers to the owner of this class, so it's attrivutes can be used.
+     */
     public SpeakMeReader(Service parent) {
-//    @Override
-//    public void onCreate() {
-//        super.onCreate();
-//
         this.parent = parent;
         tts = new TextToSpeech(parent.getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -67,32 +73,44 @@ public class SpeakMeReader {
         });
     }
 
-
+    /**
+     * Returns true if this service is not currently speaking, and it returns false if
+     * the utterance listener has been started but not finished.
+     * @return true if not currently speaking, false otherwise.
+     */
     public boolean isDoneSpeaking() {
-        return speaking == false;
+        return !speaking;
     }
 
+    /**
+     * Detects whether the input is valid, and if so, will invoke the text-to-speech
+     * reader. Its helper method will also explicitly ensure that the system volume is unmuted,
+     * as the main SpeakMe app may mute it temporarily.
+     * @param text The text to pass to the tts reader
+     */
     public void invokeTTS(String text) {
         if (text != null && text.trim().length() != 0) {
+            speaking = true;
             textToSpeech(text);
         }
     }
 
-    public void textToSpeech(String text) {
+    /**
+     * Adds the given text to the speech queue after ensuring that the system volume is unmuted.
+     * @param text The text to pass to the tts reader
+     */
+    private void textToSpeech(String text) {
+        // Unmute both main streams
         AudioManager am = (AudioManager)parent.getSystemService(Context.AUDIO_SERVICE);
         am.setStreamMute(AudioManager.STREAM_SYSTEM,false);
         am.setStreamMute(AudioManager.STREAM_MUSIC,false);
 
+        // Use
         HashMap<String, String> voiceMap = new HashMap();
         voiceMap.put(TextToSpeech.Engine.KEY_FEATURE_NETWORK_SYNTHESIS,"true");
         voiceMap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, this.utteranceID);
 		tts.speak(text, TextToSpeech.QUEUE_FLUSH, voiceMap);
         Log.d("TTS", "finished Queueing");
 	}
-//
-//	@Override
-//	public IBinder onBind(Intent arg0) {
-//		return mBinder;
-//	}
 
 }
